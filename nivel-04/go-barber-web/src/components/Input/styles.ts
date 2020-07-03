cdeca6e04a5276d6e6fcf2e41a1aa8f260f81f50
sirmd <mate.deitos@hotmail.com>
2020-07-02 23:04:09 -0300

    Nivel 04 -> ReactJS -> Utilizando Yup para validação do form de Cadastro

--- nivel-04/go-barber-web/src/components/Input/styles.ts
+++ nivel-04/go-barber-web/src/components/Input/styles.ts
@@ -1,17 +1,24 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
 
 
-
-export const Container = styled.div`
+export const Container = styled.div<ContainerProps>`
 
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
   display: flex;
   align-items: center;
-  color: #666360;
+
+  border: 2px solid;
+  ${props => props.isFocused ? `color: #ff9000` : `color: #666360`};
+  ${props => props.isFilled && `color: #ff9000`};
+  ${props => props.isFocused ? `border-color: #ff9000` : `border-color: #232129`};
 
   & + div {
     margin-top: 8px;
--- nivel-04/go-barber-web/src/pages/SignUp/index.tsx
+++ nivel-04/go-barber-web/src/pages/SignUp/index.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Container, Content, Background } from './styles';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import Input from '../../components/Input/index';
 import Button from '../../components/Button/index';
 import logo from '../../assets/logo.svg'
 const SignUp: React.FC = () => {
 
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object): Promise<void> => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string().required('O e-mail é obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'A senha deve ter no mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
 
   return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="GoBarber" />
-        <Form initialData={{ name: 'Mateus' }} onSubmit={handleSubmit}  >
+        <Form initialData={{}} onSubmit={handleSubmit}  >
           <h1>
             Faça seu cadastro
           </h1>
