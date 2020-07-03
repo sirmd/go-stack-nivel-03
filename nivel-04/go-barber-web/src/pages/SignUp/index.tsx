38ed798f76ec143a5dfad7616d3b1ea606b5d7ba
sirmd <mate.deitos@hotmail.com>
2020-07-02 22:13:12 -0300

    Nivel 04 -> ReactJS -> Utilizando Unform para referenciar campos do form

--- nivel-04/go-barber-web/src/pages/SignUp/index.tsx
+++ nivel-04/go-barber-web/src/pages/SignUp/index.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import { Container, Content, Background } from './styles';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import Input from '../../components/Input/index';
 import Button from '../../components/Button/index';
 import logo from '../../assets/logo.svg'
-const SignUp = () => {
+const SignUp: React.FC = () => {
+
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="GoBarber" />
-        <form >
+        <Form initialData={{ name: 'Mateus' }} onSubmit={handleSubmit}  >
           <h1>
             Faça seu cadastro
           </h1>
 
-          <Input icon={FiUser} name="nome" placeholder="Nome" />
+          <Input icon={FiUser} name="name" placeholder="Nome" />
           <Input icon={FiMail} name="email" placeholder="E-mail" />
           <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
 
           <Button type="submit">Cadastrar</Button>
 
-        </form>
+        </Form>
 
         <a href="">
           <FiArrowLeft />
