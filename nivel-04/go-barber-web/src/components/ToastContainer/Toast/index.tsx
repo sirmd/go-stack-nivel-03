fdb610a0786cab0a8d471d502481c42491c65599
sirmd <mate.deitos@hotmail.com>
2020-07-06 21:45:06 -0300

    Nivel 03 -> ReactJS -> Animação para Toasts

--- nivel-04/go-barber-web/src/components/ToastContainer/Toast/index.tsx
+++ nivel-04/go-barber-web/src/components/ToastContainer/Toast/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import React, { useEffect } from 'react';
 import {
   FiAlertCircle,
@@ -10,6 +11,7 @@ import { Container } from './styles';
 
 interface ToastProps {
   toast: ToastMessage;
+  style: object;
 }
 
 const icons = {
@@ -18,7 +20,7 @@ const icons = {
   success: <FiCheckCircle size={24} />,
 };
 
-const Toast: React.FC<ToastProps> = ({ toast }) => {
+const Toast: React.FC<ToastProps> = ({ toast, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -35,7 +37,11 @@ const Toast: React.FC<ToastProps> = ({ toast }) => {
   }, [removeToast, toast.id]);
 
   return (
-    <Container type={toast.type} hasDescription={!!toast.description}>
+    <Container
+      type={toast.type}
+      hasDescription={!!toast.description}
+      style={style}
+    >
       {icons[toast.type || 'info']}
       <div>
         <strong>{toast.title}</strong>
--- nivel-04/go-barber-web/src/components/ToastContainer/index.tsx
+++ nivel-04/go-barber-web/src/components/ToastContainer/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { useTransition } from 'react-spring';
 import Toast from './Toast';
 import { Container } from './styles';
 import { ToastMessage } from '../../hooks/ToastContext';
@@ -9,10 +11,20 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 0 },
+    },
+  );
+
   return (
     <Container>
-      {messages.map(message => (
-        <Toast key={message.id} toast={message} />
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} toast={item} />
       ))}
     </Container>
   );
