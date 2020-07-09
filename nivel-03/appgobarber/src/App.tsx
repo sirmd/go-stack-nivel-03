3b7e90b1196dbaa0455aeae80e31b0f962693b7b
sirmd <mate.deitos@hotmail.com>
2020-07-08 22:36:15 -0300

    Nivel 03 -> ReactNative -> Commit inicial configurando status bar.

---
+++ nivel-03/appgobarber/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38"/>
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Text>Hello Mateus</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
