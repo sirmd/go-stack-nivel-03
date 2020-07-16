202eb5eadd654a24a0cf7269278a3cd4128af0f9
sirmd <mate.deitos@hotmail.com>
2020-07-16 20:30:04 -0300

    Nivel 03 -> ReactNative -> Importando a logo

--- nivel-03/appgobarber/src/pages/SignIn/index.tsx
+++ nivel-03/appgobarber/src/pages/SignIn/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { Image } from 'react-native';
 import { Container } from './styles';
+import logoImg from '../../../assets/logo.png';
 
 const SignIn: React.FC = () => {
-
-  return <Container />;
+  return (<Container >
+    <Image source={logoImg} />
+  </Container>);
 }
 
 export default SignIn;
